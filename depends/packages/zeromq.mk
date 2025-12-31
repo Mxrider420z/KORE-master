886ef1c1479e80b0329f3c278cd1e42c0bb960a9
Mxrider420z <mxrider008@gmail.com>
2025-12-31 15:50:42 -0300

    depends: Fix zeromq package build configuration
    Use standard autoconf macro for proper cross-compilation support.
    The previous manual configure invocation was missing critical flags
    for prefix, host, CFLAGS, and LDFLAGS. Also adds mingw32 support
    and cleans up documentation from the build output.

--- depends/packages/zeromq.mk
+++ depends/packages/zeromq.mk
@@ -5,21 +5,26 @@ $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=6653ef5910f17954861fe72332e68b03ca6e4d9c7160eb3a8de5a5a913bfab43
 
 define $(package)_set_vars
-$(package)_config_opts=--without-docs --disable-shared --enable-static --disable-dependency-tracking --without-libsodium --enable-curve=no --disable-libunwind --disable-perf --disable-Werror
+$(package)_config_opts=--without-docs --disable-shared --enable-static
+$(package)_config_opts+=--disable-dependency-tracking --without-libsodium
+$(package)_config_opts+=--disable-curve --disable-libunwind --disable-perf
+$(package)_config_opts+=--disable-Werror --disable-drafts
 $(package)_config_opts_linux=--with-pic
+$(package)_config_opts_mingw32=--enable-static --disable-shared
 endef
 
 define $(package)_config_cmds
-	./configure $(host) $($(package)_config_opts)
+  $($(package)_autoconf)
 endef
 
 define $(package)_build_cmds
-	$(MAKE)
+  $(MAKE)
 endef
 
 define $(package)_stage_cmds
-	$(MAKE) DESTDIR=$($(package)_staging_dir) install
-	mkdir -p $($(package)_staging_dir)$(host_prefix)/lib
-	# FIX: Search for libzmq.a and force-install it to the correct staging path
-	find . -name libzmq.a -exec install -D -m 644 {} $($(package)_staging_dir)$(host_prefix)/lib/libzmq.a \;
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install
+endef
+
+define $(package)_postprocess_cmds
+  rm -rf share
 endef
